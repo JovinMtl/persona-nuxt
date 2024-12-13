dd2fde368e254c0b86a80b554b0d1c9f027af6ef
JovinMtl <nsanzumukizath@gmail.com>
2024-12-13 16:53:20 +0200

    commenting out the default components in the main app

--- app.vue
+++ app.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <!-- <NuxtRouteAnnouncer /> -->
+    <!-- <NuxtWelcome /> -->
+     muteule
+      <NuxtPage/>
   </div>
 </template>
