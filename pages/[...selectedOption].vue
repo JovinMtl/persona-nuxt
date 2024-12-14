273ca0bbe1806a06621606e87800855b709625d0
JovinMtl <nsanzumukizath@gmail.com>
2024-12-14 22:20:57 +0200

    made component that will be the base for routing

---
+++ pages/[...selectedOption].vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <hea-der></hea-der>
+        <div style="background-color: blue;
+            position: absolute; top: 5px;
+            text-align: center;
+            width: 100vw;">{{ title_jove }}</div>
+        <div>
+            <me-nu></me-nu>
+        </div>
+        
+        <br>THis is my Hello world, Thierry Nsanzumukiza</div>
+    <!-- <NuxtPage/> -->
+</template>
+<script setup lang="ts">
+import heaDer from '../views/hea-der.vue';
+import meNu from '~/views/me-nu.vue';
+
+const title_jove = "Welcome to the official website of Thierry"
+</script>
