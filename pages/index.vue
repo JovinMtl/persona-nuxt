5e9b9e51efe81798522ccff28c52feb267f520ea
JovinMtl <nsanzumukizath@gmail.com>
2024-12-25 23:32:37 +0200

    renaming from selectedOption into index

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,33 @@
 <template>
     <div>
-        <hea-der></hea-der>
+        <div style="width: 100%;height: 30px"></div>
+        <div style="width: 100%;height: 40px;display: flex;
+            justify-content: center; align-items: center;
+            margin-bottom: 15px;">{{ title_jove }}</div>
         <me-nu></me-nu>
-        <br>THis is my Hello world, Thierry Nsanzumukiza</div>
+        <div v-if="choiceMenu==1" style="
+            position: relative;
+            text-align: center;
+            margin: 20px;
+            width: 100vw;">
+            <summ-ary></summ-ary>
+        </div>
+        <div v-if="choiceMenu==2" style="
+            position: relative;
+            text-align: center;
+            margin: 20px;
+            width: 100vw;">
+            <port-folio></port-folio>
+        </div>
+        
+        <!-- <br>THis is my Hello world, Thierry Nsanzumukiza -->
+    </div>
     <!-- <NuxtPage/> -->
 </template>
 <script setup lang="ts">
-import heaDer from '../views/hea-der.vue';
 import meNu from '~/views/me-nu.vue';
+import summAry from '~/views/summ-ary.vue';
+import portFolio from '~/views/port-folio.vue';
+
+const title_jove = "Welcome to the official website of Thierry"
 </script>
