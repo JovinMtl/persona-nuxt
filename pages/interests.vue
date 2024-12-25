8020df7cc5f6e39b49861187bc3714a3983ccc63
JovinMtl <nsanzumukizath@gmail.com>
2024-12-25 23:30:51 +0200

    made interest page

---
+++ pages/interests.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <div style="width: 100%;height: 30px"></div>
+        <div style="width: 100%;height: 40px;display: flex;
+            justify-content: center; align-items: center;
+            margin-bottom: 15px;">{{ title_jove }}</div>
+        <me-nu></me-nu>
+        <div v-if="choiceMenu==1" style="
+            position: relative;
+            text-align: center;
+            margin: 20px;
+            width: 100vw;">
+            <summ-ary></summ-ary>
+        </div>
+        <div v-if="choiceMenu==2" style="
+            position: relative;
+            text-align: center;
+            margin: 20px;
+            width: 100vw;">
+            <port-folio></port-folio>
+        </div>
+        
+        <!-- <br>THis is my Hello world, Thierry Nsanzumukiza -->
+    </div>
+    <!-- <NuxtPage/> -->
+</template>
+<script setup lang="ts">
+import meNu from '~/views/me-nu.vue';
+import summAry from '~/views/summ-ary.vue';
+import portFolio from '~/views/port-folio.vue';
+
+const title_jove = "You are seeing my achievements"
+</script>
