06d8592e64aac0e3ff0cd594780642ce6ed78f23
JovinMtl <nsanzumukizath@gmail.com>
2024-12-27 21:09:09 +0200

    fixed the styles for summary component

--- views/sum-mary.vue
+++ views/sum-mary.vue
@@ -35,81 +35,75 @@
         </div>
     </div>
 </template>
-<script lang="ts">
-import { ref, defineComponent } from 'vue'
+<script setup lang="ts">
+import { ref } from 'vue'
 import { Intro } from '../auxiliaire/types'
-export default defineComponent ({
-    setup() {
-        const turnDetail = (index:number)=>{
-            //will enable or disable the content to be expanded
-            (contents.value[index]).detail = !(contents.value[index]).detail
-        }
 
-        const contents:Intro = ref([
-            {
-                'title': 'About Me',
-                'content': `A Full Stack Developer with 5+ years of experience. Proficient
-                    in Python (Django DRF), JS (Vue.js + Ts), Ionic, HTML and
-                    CSS. Committed to learn and impact a successful experience
-                    in Web Development.
-            `,
-                'detail':false
-            },
-            {
-                'title':'Tech Passion and Learning Journey',
-                'content' : `
+const turnDetail = (index:number)=>{
+    //will enable or disable the content to be expanded
+    (contents.value[index]).detail = !(contents.value[index]).detail
+}
+
+const contents:Intro = ref([
+    {
+        'title': 'About Me',
+        'content': `A Full Stack Developer with 5+ years of experience. Proficient
+            in Python (Django DRF), JS (Vue.js + Ts), Ionic, HTML and
+            CSS. Committed to learn and impact a successful experience
+            in Web Development.
+    `,
+        'detail':false
+    },
+    {
+        'title':'Tech Passion and Learning Journey',
+        'content' : `
 I wrote my first "Hello world" in 2014 using C programming language at High school.
 Same my passion for Linux was ignited, leading me to explore the system extensively, 
-                often substituting formal courses with self-study using Linux books at the library. 
-                The year 2015 introduced me to Vim, UNIX commands, and Bash scripting, 
-                expanding my technical toolkit.`,
-                'detail':false
-            },
-            {
-                'title': 'Collaborative Spirit with Jonathan NKURUNZIZA',
-                'content':`My partnership with my close friend <b class='mk' style="font-size:1rem">Jonathan NKURUNZIZA</b>
-                since 2012, has been instrumental in my growth in the field of computer science. Together, 
-                we have shared knowledge, motivation, and support, shaping my journey.`,
-                'detail':false
-            },
-            {
-                'title': 'Professional Milestone: C Programming Project',
-                'content' : `A pivotal moment in my career was working on a significant project in 2017, focusing on C programming. 
-                This experience deepened my understanding of <b class='mk'>POINTERS</b> and the responsibility 
-                that comes with navigating computer memory, a realm typically reserved for C/C++ experts.
-           `,
-                'detail':false
-            }, 
-            {
-                'title':'Diversification into Commerce and Marketing',
-                'content':`From 2018 to 2021, I ventured into studying Commerce and Marketing at the University of Burundi to complement my skill set. 
-                This exploration introduced me to various crucial areas such as Ethics, Economics, Consumer Behavior, 
-                Citizenship Values, Public Administration, and Marketing Principles. 
-                It also emphasized the importance of personal development to bridge the gap between my technical and business expertise.
-           `,
-                'detail':false
-            },
-            {
-                'title':'Embracing Freelancing as a Fusion of Skills',
-                'content':`With this diverse background and a blend of technical proficiency and business acumen, I am ready to address challenges
-                 and make impact as a Full Stack Developer.
-          `,
-                'detail':false
-            }, 
-            {
-                'title':'Engage with Me',
-                'content':`Let's connect to explore synergies and opportunities 
-                    for collaboration in the dynamic realm of web development.`,
-                'detail':false
-            }
-        ])
-        return {
-            contents,
-            turnDetail, titleAlert,
-        }
+        often substituting formal courses with self-study using Linux books at the library. 
+        The year 2015 introduced me to Vim, UNIX commands, and Bash scripting, 
+        expanding my technical toolkit.`,
+        'detail':false
     },
-})
+    {
+        'title': 'Collaborative Spirit with Jonathan NKURUNZIZA',
+        'content':`My partnership with my close friend <b class='mk' style="font-size:1rem">Jonathan NKURUNZIZA</b>
+        since 2012, has been instrumental in my growth in the field of computer science. Together, 
+        we have shared knowledge, motivation, and support, shaping my journey.`,
+        'detail':false
+    },
+    {
+        'title': 'Professional Milestone: C Programming Project',
+        'content' : `A pivotal moment in my career was working on a significant project in 2017, focusing on C programming. 
+        This experience deepened my understanding of <b class='mk'>POINTERS</b> and the responsibility 
+        that comes with navigating computer memory, a realm typically reserved for C/C++ experts.
+    `,
+        'detail':false
+    }, 
+    {
+        'title':'Diversification into Commerce and Marketing',
+        'content':`From 2018 to 2021, I ventured into studying Commerce and Marketing at the University of Burundi to complement my skill set. 
+        This exploration introduced me to various crucial areas such as Ethics, Economics, Consumer Behavior, 
+        Citizenship Values, Public Administration, and Marketing Principles. 
+        It also emphasized the importance of personal development to bridge the gap between my technical and business expertise.
+    `,
+        'detail':false
+    },
+    {
+        'title':'Embracing Freelancing as a Fusion of Skills',
+        'content':`With this diverse background and a blend of technical proficiency and business acumen, I am ready to address challenges
+            and make impact as a Full Stack Developer.
+    `,
+        'detail':false
+    }, 
+    {
+        'title':'Engage with Me',
+        'content':`Let's connect to explore synergies and opportunities 
+            for collaboration in the dynamic realm of web development.`,
+        'detail':false
+    }
+])
 </script>
+
 <style scoped>
 h3{
     font-weight: bold;
@@ -172,10 +166,21 @@ h3{
     justify-content: center;
     padding: 0 5px;
 }
+
+.elementTitle{
+    height: 30px;
+    margin-top: -5px;
+    background-color: #ef4b38;
+    border-top-left-radius: 18px; 
+    border-top-right-radius: 18px;
+    text-align: center; 
+    font-size: .9rem;
+    align-items: center; 
+    align-content: center;
+}
 .elementTitle1{
     background-color: grey;
 }
-
 
 
 @media screen and (max-width: 600px) {
